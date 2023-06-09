a23d575bcc83296b8e8a840cfe1bd40b18b06ce2
zaycevalexander <zaycevalexander.v@gmail.com>
2023-06-09 04:31:19 -0600

    init

---
+++ mysite/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'mysite/post/list.html', {'post': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'mysite/post/detail.html', {'post': post})
